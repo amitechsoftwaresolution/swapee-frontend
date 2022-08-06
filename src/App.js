2f093c0d6e2ddd26e170fb9427be9702196511cb
Pragashraj <pragashraj@smartzi.com>
2022-08-06 21:11:19 +0530

    feat: footer component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import NavigationComponent from './Components/Navigation/NavigationComponent'
+import Footer from './Components/Footer/Footer'
+import Home from './Pages/Home/Home'
 
 const App = () => {
     return (
       <div>
         <NavigationComponent />
+        <Home />
+        <Footer />
       </div>
     )
 }
