3bea9cf97559577e81fe5393d0f2c55e8ea50599
Pragashraj <pragashraj@smartzi.com>
2022-08-17 19:15:10 +0530

    feat: about us page[ongoing]

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -3,10 +3,27 @@ import React, { Component } from 'react'
 import './AboutUs.css'
 
 class AboutUs extends Component {
+
+    renderHeaderParallax = () => {
+        return (
+            <div className = 'header-parallax'>
+                <span className = 'header-parallax-title'>About Us</span>
+            </div>
+        )
+    }
+
+    renderMainContainer = () => {
+        return (
+            <div className = 'about-us-main-container'>
+                { this.renderHeaderParallax() }
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className = 'about-us-page-root'>
-                
+                { this.renderMainContainer() }
             </div>
         )
     }
